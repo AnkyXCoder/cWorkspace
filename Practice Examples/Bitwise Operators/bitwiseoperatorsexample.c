089f269b4dd4d08a5fe80bc73567a30b0ef957ff
AnkyXCoder <ankit.5036@gmail.com>
2019-12-28 09:15:42 +0530

    First Try

---
+++ Practice Examples/Bitwise Operators/bitwiseoperatorsexample.c
@@ -0,0 +1,19 @@
+/* Write a program which takes two integers from the user, computes bitwise &, |, ^ and ~ and then prints the result. */
+
+#include <stdio.h>
+
+int main() {
+
+	__int32_t num1, num2;
+	printf("Enter 2 numbers separated by a space: \n");
+	scanf ("%d %d", &num1, &num2);
+	
+	printf("Bitwise AND(&): %d\n", (num1 & num2));
+	printf("Bitwise OR(|): %d\n", (num1 | num2));
+	printf("Bitwise XOR(^): %d\n", (num1 ^ num2));
+	printf("Bitwise NAGATE(~): %d and %d\n",(~num1), (~num2));
+	printf("Logical AND(&&): %d\n", (num1 && num2));
+	printf("Logical OR(||): %d\n", (num1 || num2));
+	printf("Logical XOR(^): %d\n", (num1 ^ num2));
+	printf("Logical NAGATE(!): %d and %d\n",(!num1), (!num2));
+}
